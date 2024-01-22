42b7575cfd6563dc665c5fee9e73a55724faf774
Vishnu Vishweshwar <VISH396534@exactsoftware.com>
2024-01-22 10:44:47 +0530

    Initial commit

---
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.Logging;
+using Moq;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Tests
+{
+    public class Tests
+    {
+        WeatherForecastController weatherForecastController;
+        Mock<ILogger<WeatherForecastController>> loggerMock = new Mock<ILogger<WeatherForecastController>>();
+        [SetUp]
+        public void Setup()
+        {
+            weatherForecastController = new WeatherForecastController(loggerMock.Object);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            var results = weatherForecastController.Get();
+            Assert.That(results.Count, Is.EqualTo(6));
+        }
+    }
+}
